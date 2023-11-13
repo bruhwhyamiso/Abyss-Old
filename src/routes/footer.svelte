a3be018d9a556ee9948740d73ecfba7a1cd2a14d
impaxton <hey@paxton.rip>
2023-11-13 13:36:49 -0500

    Update footer.svelte

--- src/routes/footer.svelte
+++ src/routes/footer.svelte
@@ -1,6 +1,6 @@
 <footer>
-	<a href="https://discord.gg/goabyss" target="_blank">Discord</a>
-	<a href="https://github.com/Abyss-Services" target="_blank">GitHub</a>
+	<a href="https://intoabyss.org/discord" target="_blank">Discord</a>
+	<a href="https://intoabyss.org/github" target="_blank">GitHub</a>
 	<a href="https://intoabyss.org/apply" target="_blank">Apply for Staff</a>
 	<p>Copyright Abyss Services 2023</p>
 </footer>
