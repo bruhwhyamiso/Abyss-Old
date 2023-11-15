949c088f7b000939128b99d807f9484b265dbd04
impaxton <hey@paxton.rip>
2023-11-14 20:24:52 -0500

    Update footer.svelte

--- src/routes/footer.svelte
+++ src/routes/footer.svelte
@@ -2,5 +2,5 @@
 	<a href="https://intoabyss.org/discord" target="_blank">Discord</a>
 	<a href="https://intoabyss.org/github" target="_blank">GitHub</a>
 	<a href="https://intoabyss.org/apply" target="_blank">Apply for Staff</a>
-	<p>Copyright Abyss Services 2023</p>
+	<p>Made by Paxton Warin</p>
 </footer>
